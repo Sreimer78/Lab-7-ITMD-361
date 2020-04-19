a62c380d0d5a9c026c0a7c1a0f8b9d9f135bd20a
Sean Reimer <sreimer@hawk.iit.edu>
2020-04-19 18:06:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function init(){
   var input = document.getElementById('entryinput');
-  var results = document.getElementById('textoutput');
-  var button = document.GetElementById('entrybutton');
+  var output = document.getElementById('textoutput');
+  var button = document.getElementById('entrybutton');
   button.addEventListener('click', function(){
-	results.innerHTML = input.value;
-	alert("Sean Reimer: " + input.value);
+    output.innerHTML = input.value;
+    alert("Sean Reimer: " + input.value);
   });
 }
 window.addEventListener('load', init);
